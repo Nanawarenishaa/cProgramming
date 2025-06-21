d3ce7d9182f8cac53b7c7d008378db40c809ef1a
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-06-21 18:11:29 +0530

    Add files via upload

--- 2DArrayEx.c
+++ 2DArrayEx.c
@@ -1,91 +1,99 @@
 #include <stdio.h>
 
 int main() {
-    int arr[2][3] = {{1,2,3},{4,5,6}};
-    printf("2D Array:\n");
-    for( int i = 0 ; i < 2 ; i++ ) {
-        for ( int j = 0 ; j < 3 ; j++ ){
-            printf(" %d ", arr[i][j]);
-        }
-        printf("\n");
-    }
+    int rows,cols,rows1,cols1;
 
 
-    int rows,cols;
-    int rows2, cols2;
-    printf("Enter rows and cols for matrix :  ");
-    scanf("%d %d" , &rows,&cols);
+    printf("Enter rows and cols for matrix 1 : ");
+    scanf("%d %d",&rows,&cols);
+
+   int matrix1[rows][cols];
+
+   printf("Enter values in matrix:\n");
+   for( int i = 0 ; i < rows ; i++ ){
+    for (int j = 0; j < cols; j++)
+    {
+         printf("Array[%d][%d]:",i,j);
+         scanf(" %d", &matrix1[i][j]);
     
-    int matrix[rows][cols];
-    
-    for( int i = 0 ; i < rows ; i++ ){
-        for( int j = 0 ; j < cols ; j++ ){
-            printf("Array[%d][%d]: " ,i, j );
-            scanf("%d", &matrix[i][j]);
-        }
     }
-    
-    printf("Enter 2nd matrix rows and cols:");
-    scanf("%d %d",&rows2,&cols2);
+   }
 
-    int matrix2[rows2][cols2];
-
-    for(int i = 0 ; i < rows2 ; i++ ){
-        for( int j = 0 ; j < cols2 ; j++){
-            printf("Array[%d][%d]: ",i,j);
-            scanf("%d",&matrix2[i][j]);
-        }
+   printf(" Entered matrix:\n");
+     for( int i = 0 ; i < rows ; i++ ){
+    for (int j = 0; j < cols; j++)
+    {
+         printf(" %d",matrix1[i][j]);
+          
     }
-
-
-
-   printf("Entered 1st matrix : \n");
-    for (int i=0 ; i < rows; i++){
-        for(int j = 0 ; j < cols ; j++ ){
-            printf(" %d", matrix[i][j]);
-
-        }
-        printf("\n");
-    }
+    printf("\n");
     
-       printf("Entered 2nd matrix : \n");
-    for (int i=0 ; i < rows2; i++){
-        for(int j = 0 ; j < cols2 ; j++ ){
-            printf(" %d", matrix2[i][j]);
+   }
 
-        }
-        printf("\n");
-    }
-    if (rows != rows2 || cols != cols2){
-        return -1;
-    } else {
+       printf("Enter rows and cols for matrix 2 : ");
+    scanf("%d %d",&rows1,&cols1);
+       int matrix2[rows1][cols1];
 
-        int result[rows][cols];
-        printf("multiplication of matrix :\n");
-       for (int i=0 ; i <rows;i++){
-            for(int j = 0 ; j < cols ; j++ ){
-              result[i][j]= matrix[i][j] * matrix2[i][j];
+   printf("Enter values in matrix:\n");
+   for( int i = 0 ; i < rows1 ; i++ ){
+    for (int j = 0; j < cols1 ; j++)
+    {
+         printf("Array[%d][%d]:",i,j);
+         scanf(" %d", &matrix2[i][j]);
     
-            }
-            printf("\n");
-        }
-    
-           for (int i=0 ; i <rows;i++){
-            for(int j = 0 ; j < cols ; j++ ){
-        printf(" %d",result[i][j]);
-            }
-        }
     }
-
-    
-
-       printf("transpose matrix: \n");
-    for (int i=0 ; i < cols ; i++){
-        for(int j = 0 ; j < rows ; j++ ){
-            printf(" %d", matrix[j][i]);
-        }
-        printf("\n");
+   }
+   
+   printf(" Entered matrix:\n");
+     for( int i = 0 ; i < rows1 ; i++ ){
+    for (int j = 0; j < cols1 ; j++)
+    {
+         printf(" %d",matrix2[i][j]);
+          
     }
+    printf("\n");
+    
+   }
 
-    return 0;
+   if (rows != rows1 || cols != cols1)
+   {
+       printf("Both matrix rows and cols doesn't match! so fail to multiply! \n");
+     
+   } else{
+   
+int result[rows][cols];
+   printf("Multiplication of two matrix:\n");
+   for (int i = 0 ; i < rows; i++)
+   {
+     for (int j = 0; j < cols ; j++)
+     {
+        result[i][j] = matrix1[i][j] * matrix2[i][j];
+     }
+     
+   }
+   
+  for (int i = 0 ; i < rows; i++)
+   {
+     for (int j = 0; j < cols ; j++)
+     {
+        printf("%d ", result[i][j]);
+     }
+   printf("\n");
+   }
+    
+   }
+
+   printf(" Transpose matrix1:\n");
+     int transpose[cols][rows];
+  for (int i = 0 ; i < cols ; i++)
+   {
+     for (int j = 0; j < rows ; j++)
+     {
+        transpose[i][j]= matrix1[j][i];
+        printf("%d ",transpose[i][j]);
+     }
+   printf("\n");
+   }
+return 0;
+
 }
