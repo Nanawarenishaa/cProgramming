f2fd86df374fca33cda680e7fcaf16490c776393
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-07-28 10:27:01 -0700

    Add files via upload

---
+++ memoryAllocation.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+   
+    int* arr = (int*) malloc(n * sizeof(int));
+
+
+    if (arr == NULL) {
+        printf("Memory not allocated!\n");
+        return 1;
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = i * 10;
+    }
+
+
+    printf("Array elements:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+
+    free(arr);
+    return 0;
+}
